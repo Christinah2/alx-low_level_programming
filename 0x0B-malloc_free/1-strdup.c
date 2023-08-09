2ade65350efc1416abb4a3bea82a65814ef6309f
Christinah2 <christijoj@gmail.com>
2023-08-08 19:12:01 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,24 +8,22 @@
  * @str: char
  * Return a pointer to the string that was duplicated
  * Return as NULL if insufficient memory was available
- * Return: Always 0
+ * Return: 0
  */
 
 char *_strdup(char *str)
 {
-char *copy;
-int i, len = 0;
-
-copy = malloc(sizeof(char) * (i + 1));
+char *aaa;
+int i, r = 0;
+i = 0;
+aaa = (char *)malloc(sizeof(char) * (i + 1));
 if (str == NULL)
 return (NULL);
-if (copy == NULL)
+if (aaa == NULL)
 return (NULL);
-i = 0;
-while (str(i)i = '\0')
+while (str[i] != '\0')
 i++;
-for (i = 0; i < len; i++)
-copy(i) = str(i);
-copy(len) = '\0';
-return (copy);
+for (r = 0; str[r]; r++)
+aaa[r] = str[r];
+return (aaa);
 }
