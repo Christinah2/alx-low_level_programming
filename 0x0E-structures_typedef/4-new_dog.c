1b0c8bf217df1135c678514f1e838b543a00f1e7
Christinah2 <christijoj@gmail.com>
2023-08-15 16:11:34 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -3,6 +3,37 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Gets the string length
+ * @str: To measure the string
+ * NULL on failure
+ * Return: The string length
+ */
+
+int _strlen(char *str)
+{
+int len = 0;
+while (*str++)
+len++;
+return (len);
+}
+
+/**
+ * _strcopy - The string pointed to by src is copied
+ * @dest: Copy of the string stored in the buffer pointed to the dest
+ * @src: Copy the sring pointed to by src
+ * Return: Pointer to dest
+ */
+
+char *_strcopy(char *dest, char *src)
+{
+int index = 0;
+for (index = 0; src[index]; index++)
+dest[index] = src[index];
+dest[index] = '\0';
+return (dest);
+}
+
+/**
  * new_dog - It creates a new dog
  * @name: The dog's name
  * @age: The dog's age
