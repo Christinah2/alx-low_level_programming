5093da17f48bff8fdc00e27c2356b5af5492ffe7
Christinah2 <christijoj@gmail.com>
2023-08-15 15:16:28 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,10 +17,12 @@ if (d->name == NULL)
 printf("name: (nil)\n");
 else
 printf("name: %s\n", d->name);
+
 if (d->age < 0)
 printf("age: (nil)\n");
 else
 printf("age: %f\n", d->age);
+
 if (d->owner == NULL)
 printf("owner: (nil)\n");
 else
